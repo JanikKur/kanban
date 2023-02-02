76d069d806782340d27fa8f0f2106aa4309c2240
Janik Kurtz <janik.kurtz2@googlemail.com>
2023-02-02 11:18:57 +0100

    Fixed bug that subtask gets deleted when pressed enter

--- src/layouts/AddTaskModal.tsx
+++ src/layouts/AddTaskModal.tsx
@@ -19,7 +19,7 @@ export default function AddTaskModal({
   ) => void;
   handleClose: () => void;
 }) {
-  const [subTasks, setSubTasks] = useState<string[]>([""]);
+  const [subTasks, setSubTasks] = useState<string[]>([]);
   const titleRef = useRef<HTMLInputElement>(null!);
   const descriptionRef = useRef<HTMLTextAreaElement>(null!);
   const statusRef = useRef<HTMLSelectElement>(null!);
@@ -33,79 +33,88 @@ export default function AddTaskModal({
 
   return (
     <>
-    {show ? 
-    <Modal show={show} handleClose={handleClose} className="add-modal">
-      <h2>Add New Task</h2>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(
-            titleRef.current.value,
-            descriptionRef.current.value,
-            statusRef.current.value,
-            subTasks
-          );
-          setSubTasks([]);
-          handleClose();
-        }}
-      >
-        <div className="form-group">
-          <label>Title</label>
-          <input
-            type="text"
-            ref={titleRef}
-            placeholder="e.g. Take coffee break"
-            autoFocus
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Description</label>
-          <textarea
-            ref={descriptionRef}
-            placeholder="e.g. It's always good to take a break. This 15 minute break will reacharge the batteries a little."
-            required
-          />
-        </div>
-        <div className="form-group subtasks">
-          <label>Subtasks</label>
-          {subTasks.map((subTask, idx) => (
-            <SubtaskInput
-              key={idx}
-              value={subTask}
-              onChange={(newText: string) => updateSubTask(subTask, newText)}
-              onDelete={(text) =>
-                setSubTasks((prev: string[]) => [
-                  ...prev.filter((task: string) => task !== text),
-                ])
-              }
-            />
-          ))}
-          <button
-            className="add-subclass-btn"
-            onClick={(e) => {
+      {show ? (
+        <Modal show={show} handleClose={handleClose} className="add-modal">
+          <h2>Add New Task</h2>
+          <form
+            onSubmit={(e) => {
               e.preventDefault();
-              setSubTasks((prev) => [...prev, ""]);
+              handleSubmit(
+                titleRef.current.value,
+                descriptionRef.current.value,
+                statusRef.current.value,
+                subTasks
+              );
+              setSubTasks([]);
+              handleClose();
             }}
           >
-            + Add New Subtask
-          </button>
-        </div>
-        <div className="form-group">
-          <label>Column</label>
-          <select ref={statusRef}>
-            {statuses.map((status, idx) => (
-              <option key={idx} value={status.title}>
-                {status.title}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button type="submit" className="btn-primary">
-          Create Task
-        </button>
-      </form>
-    </Modal> : ""}
+            <div className="form-group">
+              <label>Title</label>
+              <input
+                type="text"
+                ref={titleRef}
+                placeholder="e.g. Take coffee break"
+                autoFocus
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Description</label>
+              <textarea
+                ref={descriptionRef}
+                placeholder="e.g. It's always good to take a break. This 15 minute break will reacharge the batteries a little."
+                required
+              />
+            </div>
+            <div className="form-group subtasks">
+              <label>Subtasks</label>
+              {subTasks
+                .sort()
+                .reverse()
+                .map((subTask, idx) => (
+                  <SubtaskInput
+                    key={idx}
+                    value={subTask}
+                    onChange={(newText: string) =>
+                      updateSubTask(subTask, newText)
+                    }
+                    onDelete={(text) =>
+                      setSubTasks((prev: string[]) => [
+                        ...prev.filter((task: string) => task !== text),
+                      ])
+                    }
+                  />
+                ))}
+              <button
+                type="submit"
+                className="add-subclass-btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setSubTasks((prev) => [...prev, ""]);
+                }}
+              >
+                + Add New Subtask
+              </button>
+            </div>
+            <div className="form-group">
+              <label>Column</label>
+              <select ref={statusRef}>
+                {statuses.map((status, idx) => (
+                  <option key={idx} value={status.title}>
+                    {status.title}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button type="submit" className="btn-primary">
+              Create Task
+            </button>
+          </form>
+        </Modal>
+      ) : (
+        ""
+      )}
     </>
   );
 }
@@ -123,13 +132,22 @@ function SubtaskInput({
     <div className="subtask">
       <input
         type="text"
+        autoFocus
         value={value}
-        onChange={(e) => onChange(e.target.value)}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+          }
+        }}
+        onChange={(e) => {
+          onChange(e.target.value);
+        }}
         required
       />
       <button
         onClick={(e) => {
           e.preventDefault();
+
           onDelete(value);
         }}
       >
